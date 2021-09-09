c5571c552ab11e3695f70a4ba9d5d3c2a8e48e54
MiceOnMars <danya.gansky@gmail.com>
2021-09-09 19:28:53 +0000

    Implement GetUserByID

--- go.mod
+++ go.mod
@@ -17,10 +17,11 @@ require (
 	github.com/labstack/echo/v4 v4.5.0
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/pquerna/otp v1.3.0
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/stretchr/testify v1.7.0
 	go.mongodb.org/mongo-driver v1.7.0
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
-	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	golang.org/x/sys v0.0.0-20210908233432-aa78b53d3365 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- internal/user/repository.go
+++ internal/user/repository.go
@@ -2,9 +2,9 @@ package user
 
 import (
 	"context"
-	"nnw_s/config"
 	"nnw_s/pkg/errors"
 
+	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -16,54 +16,41 @@ const templateDataExpiry = 500
 type Repository interface {
 	GetUserByID(ctx context.Context, userID string) (*User, error)
 	GetUserByEmail(ctx context.Context, email string) (*User, error)
-	GetTemplateUserDataByID(ctx context.Context, uid string) (*TemplateData, error)
-
 	CreateUser(ctx context.Context, user User) (string, error)
-	CreateTemplateUserData(ctx context.Context, templateData TemplateData) (string, error)
 }
 
 type repository struct {
 	db  *mongo.Database
-	cfg config.Config
+	log *logrus.Logger
 }
 
-func NewRepository(db *mongo.Database, cfg config.Config) *repository {
+func NewRepository(db *mongo.Database, log *logrus.Logger) *repository {
 	return &repository{
 		db:  db,
-		cfg: cfg,
+		log: log,
 	}
 }
 
 func (repo *repository) GetUserByID(ctx context.Context, userID string) (*User, error) {
-	return nil, nil
-}
-
-func (repo *repository) GetUserByEmail(ctx context.Context, email string) (*User, error) {
 	var user User
-
-	err := repo.db.Collection("user").FindOne(ctx, bson.M{"email": email}).Decode(&user)
-	if err != nil {
+	if err := repo.db.Collection("user").FindOne(ctx, bson.M{"_id": userID}).Decode(&user); err != nil {
 		if err == mongo.ErrNoDocuments {
 			return nil, ErrUserNotFound
 		}
 		return nil, errors.NewInternal(err.Error())
 	}
-
 	return &user, nil
 }
 
-func (repo *repository) GetTemplateUserDataByID(ctx context.Context, uid string) (*TemplateData, error) {
-	var templateUser TemplateData
-
-	err := repo.db.Collection("templateUserData").FindOne(ctx, bson.M{"uid": uid}).Decode(&templateUser)
-	if err != nil {
+func (repo *repository) GetUserByEmail(ctx context.Context, email string) (*User, error) {
+	var user User
+	if err := repo.db.Collection("user").FindOne(ctx, bson.M{"email": email}).Decode(&user); err != nil {
 		if err == mongo.ErrNoDocuments {
-			return nil, ErrTemplateDataNotFound
+			return nil, ErrUserNotFound
 		}
 		return nil, errors.NewInternal(err.Error())
 	}
-
-	return &templateUser, nil
+	return &user, nil
 }
 
 func (repo *repository) CreateUser(ctx context.Context, user User) (string, error) {
@@ -82,23 +69,4 @@ func (repo *repository) CreateUser(ctx context.Context, user User) (string, erro
 		return "", errors.NewInternal(err.Error())
 	}
 	return user.ID.String(), nil
-}
-
-func (repo *repository) CreateTemplateUserData(ctx context.Context, templateData TemplateData) (string, error) {
-	mod := mongo.IndexModel{
-		Keys:    bson.M{"created_at": 1}, // index in ascending order or -1 for descending order
-		Options: options.Index().SetExpireAfterSeconds(templateDataExpiry),
-	}
-
-	_, err := repo.db.Collection("templateUserData").Indexes().CreateOne(ctx, mod)
-	if err != nil {
-		return "", errors.NewInternal(err.Error())
-	}
-
-	_, err = repo.db.Collection("templateUserData").InsertOne(ctx, templateData)
-	if err != nil {
-		return "", errors.NewInternal(err.Error())
-	}
-
-	return templateData.Uid, nil
 }
