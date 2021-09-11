272b9b59cfdce2b4fb4cb3106eb476de1c32b327
RomanBarashkov <4eepHr7AiDjaWpv>
2021-09-11 20:46:02 +0300

    comments added to the code

--- pkg/wallet/menmonic_decryptor.go
+++ pkg/wallet/menmonic_decryptor.go
@@ -6,18 +6,22 @@ import (
 )
 
 func Decrypt(data []byte, passphrase string) []byte {
+	// Create passphrase hash key
 	key := []byte(createHash(passphrase))
 
+	// Create a new block cipher based on the hashed passphrase
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
 	}
 
+	// Wrap block in (GCM) with a standard nonce length.
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
 
+	// Separating the nonce and the encrypted data.
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
 
--- pkg/wallet/mnemonic_ecryptor.go
+++ pkg/wallet/mnemonic_ecryptor.go
@@ -9,6 +9,7 @@ import (
 	"io"
 )
 
+// Hash key, then return the hash as a hexadecimal value.
 func createHash(key string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(key))
@@ -16,18 +17,22 @@ func createHash(key string) string {
 }
 
 func Encrypt(data []byte, passphrase string) []byte {
+	// Create a new block cipher based on the hashed passphrase
 	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
 
+	// Wrap block in (GCM) with a standard nonce length.
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
 
+	// Create a nonce.
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
 		panic(err.Error())
 	}
 
+	// Append the nonce to the encrypted data
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
 
 	return ciphertext
