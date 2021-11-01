10133f323766cb8007552301a21ac00e42d872e5
RomanBarashkov <4eepHr7AiDjaWpv>
2021-11-01 22:10:14 +0200

    in progress

--- pkg/wallet/Ethereum/wallet_test.go
+++ pkg/wallet/Ethereum/wallet_test.go
@@ -1,8 +1,11 @@
 package Ethereum
 
 import (
+	"context"
+	"crypto/ecdsa"
 	"fmt"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 	"github.com/tyler-smith/go-bip39"
@@ -10,49 +13,98 @@ import (
 	"testing"
 )
 
-func TestGenerateEthHDWallet(t *testing.T) {
+func TestGenerateEthHDWalletAndMakeTransaction(t *testing.T) {
+	entropy, _ := bip39.NewEntropy(256)
+	mnemonic, _ := bip39.NewMnemonic(entropy)
+
+	privateKeyECDSA, err := createHdWallet(mnemonic)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	client, err := ethclient.Dial("http://localhost:8545")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	fmt.Println("we have a connection")
-	_ = client // we'll use this in the upcoming sections
-}
-
-func TestGenerateEthHDWalletAndMakeTransaction(t *testing.T) {
-	entropy, _ := bip39.NewEntropy(256)
-	mnemonic, _ := bip39.NewMnemonic(entropy)
-
-	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
+	ctx := context.Background()
+	chainID, err := client.ChainID(ctx)
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	gasTip, err := client.SuggestGasTipCap(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	blockNumber, err := client.BlockNumber(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	nonce, err := client.NonceAt(ctx, crypto.PubkeyToAddress(privateKeyECDSA.PublicKey), new(big.Int).SetUint64(blockNumber))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	value := big.NewInt(1000000000000000000)
+	toAddress := crypto.PubkeyToAddress(privateKeyECDSA.PublicKey)
+	gasLimit := uint64(21000)
+
+	fmt.Println("toAddress:", &toAddress)
+
+	tx := types.NewTx(&types.DynamicFeeTx{
+		ChainID:   chainID,
+		Nonce:     nonce,
+		GasFeeCap: gasPrice,
+		GasTipCap: gasTip,
+		Gas:       gasLimit,
+		To:        &toAddress,
+		Value:     value,
+		Data:      []byte{},
+	})
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKeyECDSA)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = client.SendTransaction(ctx, signedTx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", signedTx.Hash().Hex())
+}
+
+func createHdWallet(mnemonic string) (*ecdsa.PrivateKey, error) {
+	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
+	if err != nil {
+		return nil, err
 	}
 
 	path := hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/0")
 	account, err := wallet.Derive(path, false)
 	if err != nil {
-		t.Fatal(err)
+		return nil, err
 	}
 
-	fmt.Println(account.Address.Hex())
-
-	_, err = ethclient.Dial("http://localhost:8545")
+	fmt.Printf("Account address: %s\n", account.Address.Hex())
+	privateKey, err := wallet.PrivateKeyHex(account)
 	if err != nil {
-		t.Fatal(err)
+		return nil, err
 	}
 
-	fmt.Println("we have a connection")
-	nonce := uint64(0)
-	value := big.NewInt(1000000000000000000)
-	toAddress := account.Address
-	gasLimit := uint64(21000)
-	gasPrice := big.NewInt(21000000000)
-	var data []byte
-
-	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
-	_, err = wallet.SignTx(account, tx, nil)
+	privateKeyECDSA, err := crypto.HexToECDSA(privateKey)
 	if err != nil {
-		t.Fatal(err)
+		return nil, err
 	}
+
+	return privateKeyECDSA, nil
 }
